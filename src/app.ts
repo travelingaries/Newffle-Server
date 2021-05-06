99112676716ad119f3eb47819e1b3f65afcc91df
travelingaries <edwardjhchang@gmail.com>
2021-05-06 12:16:13 +0900

    build apis

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ import dbconfig from './config/dbconfig.json';
 const pool:Pool = mysql.createPool(dbconfig);
 import {OkPacket, RowDataPacket} from "mysql";
 import ErrnoException = NodeJS.ErrnoException;
+import {find} from "tslint/lib/utils";
 
 const app = express();
 app.use(express.json());
@@ -120,6 +121,85 @@ app.post('/admin/add_news', (req: Request, res: Response) => {
             });
         });
     });
+});
+app.post('/user/categories', (req: Request, res: Response) => {
+    console.log('got here 0');
+    let data:any = req.body;
+    let insert_category_sql = "INSERT INTO user_category_subscriptions(user_idx, category_idx, status) VALUES(?, ?, ?)";
+    pool.query(insert_category_sql, [data.user_idx, data.category_idx, 1], (err, results:RowDataPacket[], fields) => {
+        if (err) {
+            console.error(err.message);
+            res.sendStatus(400);
+        }
+        console.log('got here');
+        res.sendStatus(200);
+    });
+});
+app.get('/user/find_user_idx/:uid', (req: Request, res: Response) => {
+    let uid_sql = "SELECT * FROM users WHERE firebase_uid=?";
+    pool.query(uid_sql, [req.params.uid], (err, results: RowDataPacket[], fields) => {
+       if (err) {
+           console.error('err.message');
+           res.sendStatus(400);
+       } else if(results.length < 1) {
+           res.sendStatus(404);
+       } else {
+           res.json(results[0].idx);
+       }
+    });
+});
+app.get('/user/categories/:uid', (req: Request, res: Response) => {
+    let uid:String = req.params.uid;
+    let category_sql = "SELECT * FROM `user_category_subscriptions` JOIN `users` ON users.idx=user_category_subscriptions.user_idx JOIN `news_categories` ON news_categories.idx=user_category_subscriptions.category_idx WHERE users.firebase_uid=?";
+    pool.query(category_sql, [uid], (err, results:RowDataPacket[], fields) => {
+        if (err) {
+            console.error(err.message);
+            res.sendStatus(400);
+        }
+        let user_categories:String[] = [];
+        results.forEach((result) => {
+            user_categories.push(result.category);
+        });
+        res.json(user_categories);
+    });
+});
+app.get('/news/categories', (req: Request, res: Response) => {
+    let categories_sql = "SELECT * FROM `news_categories`";
+    pool.query(categories_sql, [], (err, results:RowDataPacket[], fields) => {
+        if (err) {
+            console.error(err.message);
+            res.sendStatus(400);
+        }
+        let categories:String[] = [];
+        results.forEach((result) => {
+            categories.push(result.category);
+        });
+        res.json(categories);
+    });
+});
+app.get('/news/news_in_category/:category_idx', (req: Request, res: Response) => {
+   let news_in_categories_sql = "SELECT * FROM `news` JOIN `news_categories_map` ON news_categories_map.news_idx = news.idx WHERE news_categories_map.category_idx=?";
+   pool.query(news_in_categories_sql, [req.params.category_idx], (err, results:RowDataPacket[], fields) => {
+      if(err) {
+          console.error(err.message);
+          res.sendStatus(400);
+      }
+      res.json(results);
+   });
+});
+app.post('/news/find_category_idx', (req: Request, res: Response) => {
+   let data:any = req.body;
+   let find_category_idx_sql = "SELECT * FROM `news_categories` WHERE category=?";
+   pool.query(find_category_idx_sql, [data.category], (err, results:RowDataPacket[], fields) => {
+      if (err) {
+          console.error(err.message);
+          res.sendStatus(400);
+      } else if (results.length < 1) {
+          res.sendStatus(404);
+      } else {
+          res.json(results[0].idx);
+      }
+   });
 });
 app.post('/account/signup', (req: Request, res: Response) => {
     let data:any = req.body;
