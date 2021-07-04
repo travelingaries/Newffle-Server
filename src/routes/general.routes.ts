650a58987c9de29a538c7078c6ff0b04b1bef8a5
travelingaries <edwardjhchang@gmail.com>
2021-07-04 12:23:17 +0900

    종합 메타 함수 추가

--- src/routes/general.routes.ts
+++ src/routes/general.routes.ts
@@ -1,34 +1,61 @@
 import {Request, Response, Router} from 'express';
 import {RowDataPacket} from "mysql";
-import userRouter from "./user.routes";
 const { pool } = require('../helpers/database');
 
 const generalRouter = Router();
 
-/**
- * 메뉴 항목들 불러오기
- */
-generalRouter.get('/menu', async(req: Request, res: Response) => {
-    let drawerMenuItemsSql = "SELECT * FROM `drawer_menu_items` WHERE `status`=1";
-    try {
-        const [queryResults] = await pool.promise().query(drawerMenuItemsSql);
+generalRouter.get('/meta/:screen', async(req:Request, res:Response) => {
+    const data:any = req.body;
+    const screen:string = req.params.screen;
+    // 메인화면 메타
+    if(screen == 'main') {
+        // 현재 영업시간인지 확인
+        const date:Date = new Date();
+        let worktime:boolean = true;
+        if(date.getDay() % 6 === 0) {
+            worktime = false;
+        } else {
+            const month:number = date.getMonth();
+            const day:number = date.getDate();
+            const holidays:number[][] = [[8,16], [9,20], [9,21], [9,22], [10,4], [10,11], [12,27]];
+            for(const holiday in holidays) {
+                if((month+1) === parseInt(holiday[0]) && day === parseInt(holiday[1])) {
+                    worktime = false;
+                }
+            }
+        }
+        if(worktime) {
+            const hour:number = date.getHours();
+            if(hour < 8 || hour === 12 || hour >= 16) {
+                worktime = false;
+            }
+        }
+
+        // 메뉴 항목들 불러오기
+        let drawerMenuItemsSql = "SELECT * FROM `drawer_menu_items` WHERE `status`=1";
         let menuNames:string[] = [];
         let menuRoutes:string[] = [];
-        queryResults.forEach((result:RowDataPacket) => {
-            if(result.menu != null && result.category != '') {
-                menuNames.push(result.menu);
-            }
-            if(result.route != null && result.route != '') {
-                menuRoutes.push(result.route);
-            }
-        })
-        res.json({
-            'menuNames': menuNames,
-            'menuRoutes': menuRoutes,
-        });
-    } catch(err) {
-        console.error(err.message);
-        res.sendStatus(400);
+        try {
+            const [queryResults] = await pool.promise().query(drawerMenuItemsSql);
+            queryResults.forEach((result:RowDataPacket) => {
+                if(result.menu != null && result.category != '') {
+                    menuNames.push(result.menu);
+                }
+                if(result.route != null && result.route != '') {
+                    menuRoutes.push(result.route);
+                }
+            })
+            res.json({
+                'worktime': worktime,
+                'menuNames': menuNames,
+                'menuRoutes': menuRoutes,
+            });
+        } catch(err) {
+            console.error(err.message);
+            res.sendStatus(400);
+        }
+    } else {
+        res.sendStatus(401);
     }
 });
 
