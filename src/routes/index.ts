85ffb1a2f26de5a8889acbec3a54aa4e0aafea65
travelingaries <edwardjhchang@gmail.com>
2021-07-08 00:02:23 +0900

    메인 서버에서 admin route삭제

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import termsRouter from './terms.routes';
 import generalRouter from './general.routes';
 import accountRouter from './account.routes';
-import adminRouter from './admin.routes';
 import newsRouter from './news.routes';
 import userRouter from './user.routes';
 
@@ -10,7 +9,6 @@ const routes = Router();
 routes.use('/terms', termsRouter);
 routes.use('/general', generalRouter);
 routes.use('/account', accountRouter);
-routes.use('/admin', adminRouter);
 routes.use('/news', newsRouter);
 routes.use('/user', userRouter);
 
