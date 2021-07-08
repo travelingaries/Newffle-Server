cbb3d64a4c3b116c6a7676f27a6ad2c0bd5b2a9e
travelingaries <edwardjhchang@gmail.com>
2021-07-08 09:58:07 +0900

    다시 admin/request_add_news 살리기

---
+++ src/routes/admin.routes.ts
@@ -0,0 +1,24 @@
+import {Request, Response, Router} from 'express';
+import {findCategoryIdx, getCategories, saveNewsCategoriesMap} from "../libraries/news_library";
+import {sendNewsFcmPush} from "../libraries/push_library";
+
+const { pool } = require('../helpers/database');
+
+const adminRouter = Router();
+
+/**
+ * 운영자에게 뉴스 추가 요청
+ */
+adminRouter.post('/request_add_news', async (req: Request, res: Response) => {
+    let data:any = req.body;
+    let requestAddNewsSql = "INSERT INTO `news_add_requests`(`url`, `memo`) VALUES (?, ?)";
+    try {
+        const [insertRequestResult] = await pool.promise().query(requestAddNewsSql, [data.url, data.memo]);
+        res.sendStatus(200);
+    } catch(err) {
+        console.error(err);
+        res.sendStatus(400);
+    }
+});
+
+export default adminRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import generalRouter from './general.routes';
 import accountRouter from './account.routes';
 import newsRouter from './news.routes';
 import userRouter from './user.routes';
+import adminRouter from './admin.routes';
 
 const routes = Router();
 routes.use('/terms', termsRouter);
@@ -11,5 +12,6 @@ routes.use('/general', generalRouter);
 routes.use('/account', accountRouter);
 routes.use('/news', newsRouter);
 routes.use('/user', userRouter);
+routes.use('/admin', adminRouter);
 
 export default routes;
