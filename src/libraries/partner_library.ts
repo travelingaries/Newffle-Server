62ca0b991f0339a24a1b6c892625af7e31c996e8
travelingaries <edwardjhchang@gmail.com>
2021-07-21 14:22:45 +0900

    인사이트 정보 조회 시 idx 내리도록 작업

--- src/libraries/partner_library.ts
+++ src/libraries/partner_library.ts
@@ -1,7 +1,7 @@
 const { pool } = require('../helpers/database');
 
 export async function getLatestPartnerInsights(limit:number = -1) {
-    let searchInsightDataSql = "SELECT `partner`, `logo_image_url`, `title`, `url` FROM `insights` JOIN `partners` ON `insights`.`partner_idx`=`partners`.`idx` WHERE `partners`.`status`=1 AND `insights`.`status`=1 ORDER BY `insights`.`idx` DESC";
+    let searchInsightDataSql = "SELECT `insights`.`idx` as insight_idx, `partner`, `logo_image_url`, `title`, `url` FROM `insights` JOIN `partners` ON `insights`.`partner_idx`=`partners`.`idx` WHERE `partners`.`status`=1 AND `insights`.`status`=1 ORDER BY `insights`.`idx` DESC";
     if(limit > 0) {
         searchInsightDataSql += " LIMIT " + limit;
     }
